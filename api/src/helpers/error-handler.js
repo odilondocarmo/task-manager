df76574b0eb3047ccad8d54abb9769dc3df63442
odilondocarmo <odilonfdocarmo@gmail.com>
2022-11-26 20:40:49 -0300

    fix: error handdler behavior

--- api/src/helpers/error-handler.js
+++ api/src/helpers/error-handler.js
@@ -1,23 +1,23 @@
 export function invalidInput(message = 'Invalid input') {
     const err = new Error(message);
     err.status = 400;
-    return err;
+    throw err;
 }
 
 export function invalidPassword(message = 'Invalid password') {
     const err = new Error(message);
     err.status = 401;
-    return err;
+    throw err;
 }
 
 export function invalidUsername(message = 'Invalid user name') {
     const err = new Error(message);
     err.status = 401;
-    return err;
+    throw err;
 }
 
 export function invalidRole() {
     const err = new Error('This user has no permission');
     err.status = 403;
-    return err;
+    throw err;
 }
