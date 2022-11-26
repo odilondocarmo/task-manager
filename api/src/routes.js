7ff3a5b6b7b75c8320c8395730586985917fd9c2
odilondocarmo <odilonfdocarmo@gmail.com>
2022-11-26 20:40:34 -0300

    feat: add tasks routes

--- api/src/routes.js
+++ api/src/routes.js
@@ -1,18 +1,24 @@
 import { Router, json } from 'express';
 import cors from 'cors';
 import UsersController from './controllers/users-controller.js';
-import sessionController from './controllers/session-controller.js';
+import SessionController from './controllers/session-controller.js';
 import { isManager, isUser } from './middlewares/auth.js';
+import TaskController from './controllers/tasks-controller.js'
 
 const routes = new Router();
 
 routes.use(cors());
 routes.use(json());
 
-routes.post('/sessions', sessionController.store);
+routes.post('/sessions', SessionController.store);
 
 routes.use(isUser); //all users are allowed
-routes.get('/user/{id}', UsersController.index);
+routes.get('/users/:id', UsersController.index);
+routes.get('/tasks/:id', TaskController.index);
+routes.get('/tasks', TaskController.show);
+routes.post('/tasks', TaskController.create);
+routes.delete('/tasks/:id', TaskController.delete);
+routes.post('/tasks/perform/:id', TaskController.performTask);
 
 routes.use(isManager); //only managers
 routes.post('/user', UsersController.create);
